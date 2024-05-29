b9ccc079aff02eb2610ff9e5f15dfdf81eebc95f
Shamel Jones <ssingletary021@gmail.com>
2024-05-29 16:02:44 +0000

    refactor: Organize element display logic into DISPLAY function

--- element.sh
+++ element.sh
@@ -4,6 +4,14 @@ PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
 SYMBOL=$1
 
+
+DISPLAY() {
+  echo $DATA | while IFS=" |" read ATOMIC_NUMBER SYMBOL NAME TYPE MASS MELTING_POINT BOILING_POINT 
+do
+  echo -e "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+done
+}
+
 #if no input
 if [[ -z $1 ]]
 then 
@@ -17,17 +25,16 @@ else
     if [[ ${#SYMBOL} -gt 2 ]]
     then
       
-      # get data by full name
-      read -r atomic_number symbol name type atomic_mass melting_point_celsius boiling_point_celsius <<< $($PSQL "SELECT e.atomic_number, e.symbol, e.name, t.type, p.atomic_mass, p.melting_point_celsius, p.boiling_point_celsius FROM elements e INNER JOIN properties p ON e.atomic_number = p.atomic_number INNER JOIN types t ON p.type_id = t.type_id WHERE e.name = '$SYMBOL'")
-
+      # get data by full name 
+      DATA=$($PSQL "SELECT e.atomic_number, e.symbol, e.name, t.type, p.atomic_mass, p.melting_point_celsius, p.boiling_point_celsius FROM elements e INNER JOIN properties p ON e.atomic_number = p.atomic_number INNER JOIN types t ON p.type_id = t.type_id WHERE e.name = '$SYMBOL'")
       
-      if [[ -z $name ]]
+      
+      if [[ -z $DATA ]]
       then
         echo "I could not find that element in the database."
       else
         # display data
-        echo "full name"
-        #echo "The element with atomic number $atomic_number is $name ($symbol). It's a $type, with a mass of $atomic_mass amu. $name has a melting point of $melting_point_celsius celsius and a boiling point of $boiling_point_celsius celsius."
+        DISPLAY
       fi 
     else
       # get data by atomic_name (1 or 2 letters)
@@ -39,4 +46,5 @@ else
 
   fi
 fi
+
 exit 
