98a397fe8bde1c107bd46ad2746299479d08b49d
Shamel Jones <ssingletary021@gmail.com>
2024-05-29 13:41:35 +0000

    feat: Implement symbol input handling for data retrieval
    Add checks for symbol type and length to determine the appropriate data retrieval method

--- element.sh
+++ element.sh
@@ -2,3 +2,20 @@
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
 echo "Please provide an element as an argument."
+
+SYMBOL = $1
+
+#if input is not a number
+if [[ ! $SYMBOL =~ ^[0-9]+$ ]]
+then
+  
+  #if input is greater than two letters
+  if [[ ${#SYMBOL} -gt 2 ]]
+  then
+    # get data by full name
+  else
+    # get data by atomic_name (1 or 2 letters)
+  fi
+else
+  # get data by atomic_number
+fi
